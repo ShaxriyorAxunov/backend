30bfcdc6184c8b72bc204cf4b1685810d4a76a18
ShaxriyorAxunov <axunovshaxriyor249@gmail.com>
2025-06-01 16:25:37 +0500

    update

--- movie-tracker-backend/index.js
+++ movie-tracker-backend/index.js
@@ -9,8 +9,12 @@ const PORT = process.env.PORT || 5000;
 // PostgreSQL ulanish pooli
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
+module.exports = pool;
 app.use(cors());
 app.use(express.json());
 
