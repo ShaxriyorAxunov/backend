309c3f1fe78cbaa01bac727e6c6c79d230352d9f
ShaxriyorAxunov <axunovshaxriyor249@gmail.com>
2025-06-01 16:41:57 +0500

    push

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    hmr: process.env.NODE_ENV === 'development',
+  },
+});
